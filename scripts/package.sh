a4c80a501cf872c3739a1feb585b4cb5956e32c5
IzeLeam <luca.pourceau@gmail.com>
2025-08-30 16:32:31 +0200

    refactor: remove unused options and cleanup logic

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,67 +1,25 @@
 #!/bin/bash
 
-OUT_DIR="out"
 DIST_FILE="dist/discord-updater"
 DEBIAN_DIST_DEST="deb/usr/local/bin"
 
 VERSION=$(cat deb/DEBIAN/control | grep Version | cut -d " " -f 2)
 
 usage() {
-    echo "Usage: ./build.sh <build> [OPTIONS] (deb, rpm, arch, snap)"
+    echo "Usage: ./package.sh [OPTIONS]"
     echo ""
     echo "Options:"
     echo "  -h, --help      Show this help message"
-    echo "  -v, --verbose   Enable verbose mode"
 }
-
-if [ $# -lt 1 ]; then
-	echo "Missing build argument"
-	usage
-	exit
-fi
 
 if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
     usage
     exit
 fi
 
-if [ "$1" == "-v" ] || [ "$1" == "--verbose" ]; then
-    set -x
-fi
-
 # Copy binary to packages
 cp "$DIST_FILE" "$DEBIAN_DIST_DEST"
 
-# Create out directory if it doesn't exist or clean it
-if [ ! -d "$OUT_DIR" ]; then
-    mkdir "$OUT_DIR"
-else
-    rm -rf "$OUT_DIR"/*
-fi
-
-# Build package
-if [ "$1" == "deb" ]; then
-    echo "Building deb package"
-    dpkg-deb --build deb out/discord-updater-"$VERSION".deb
-elif [ "$1" == "rpm" ]; then
-    echo "Building rpm package"
-    echo "Not implemented yet"
-    exit
-    rpmbuild -bb rpm/SPECS/discord-updater.spec
-elif [ "$1" == "arch" ]; then
-    echo "Building arch package"
-    echo "Not implemented yet"
-    exit
-    makepkg -f
-elif [ "$1" == "snap" ]; then
-    echo "Building snap package"
-    echo "Not implemented yet"
-    exit
-    snapcraft
-else
-    echo "Invalid build argument"
-    usage
-    exit
-fi
+dpkg-deb --build --root-owner-group deb discord-updater-"$VERSION".deb
 
 echo "Build completed"
