e6ef8e5aacf9db430a2472b5f52cbef0cfff3314
IzeLeam <luca.pourceau@gmail.com>
2025-09-16 14:00:36 +0200

    fix: clean up previous builds before packaging

--- scripts/package.sh
+++ scripts/package.sh
@@ -20,6 +20,10 @@ fi
 # Copy binary to packages
 cp "$DIST_FILE" "$DEBIAN_DIST_DEST"
 
+# Remove previous builds
+rm -rf *.deb 2>/dev/null
+
+# Build package
 dpkg-deb --build --root-owner-group deb discord-updater-"$VERSION".deb
 
 echo "Build completed"
